774270b8c7819d528c7dddc3e514c72234c701e5
András Dobos <druevisual@gmail.com>
2023-05-20 15:42:52 +0200

    Clean project setup

---
+++ src/App/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "../components/Nav/Nav";
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen bg-black">
+      <Nav />
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      basecolor: "#808080",
+      black: "#000",
+      gray_border: "#D6D6D6",
+    },
+  },
+  plugins: [],
+};
