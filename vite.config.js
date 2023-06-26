1e8bc40ec45d52ac8c888ab9b20afdda9cdfa07f
András Dobos <druevisual@gmail.com>
2023-06-26 10:33:58 +0200

    add dist

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "https://druevisual.github.io/Microred",
+  base: "https://druevisual.github.io/Microred/index.html",
   plugins: [react()],
 });
