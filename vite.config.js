0a22ab822c69767a3abef842fdf33af9396bab20
András Dobos <druevisual@gmail.com>
2023-06-26 10:30:26 +0200

    base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "https://DRUEVISUAL.github.io/Microred",
+  base: "https://druevisual.github.io/Microred",
   plugins: [react()],
 });
